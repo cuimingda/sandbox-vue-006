bddd3b4072a04f34571bc80594330b029acc863b
cuimingda <cuimingda@gmail.com>
2024-10-16 14:09:40 +0800

    fix assets url path

--- apps/web/vite.config.js
+++ apps/web/vite.config.js
@@ -4,13 +4,18 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+export default defineConfig(({ mode }) => {
+  const base = mode === 'production' ? '' : '/';
+  // const base = mode === 'production' ? `/${process.env.VITE_BASE.split('/')[1]}/` : '/';
+  return {
+    base,
+    plugins: [
+      vue(),
+    ],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      }
     }
   }
 })
